1997867bbd63c9304b7121aead6296fd910c7ced
Jean Fernandine <jfernandine@gmail.com>
2022-01-12 21:37:51 -0300

    primeiro commit

---
+++ js/owl/setup.js
@@ -0,0 +1,15 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    nav:false,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:9
+        }
+    }
+})
